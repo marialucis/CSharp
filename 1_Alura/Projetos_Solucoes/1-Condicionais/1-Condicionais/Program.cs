f18253316b7c791348420dd1d8a4011ed5419c1a
Luci Sousa <marialuci148@gmail.com>
2022-03-27 23:10:31 -0300

    Subindo arquivos

---
+++ 1_Alura/Projetos_Solucoes/1-Condicionais/1-Condicionais/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeLuci = 15; /*Declarando variavel*/
+            int qtdPessoa = 2;
+
+            Console.WriteLine("Testando Condicionais");
+
+            if (idadeLuci >= 18) /*condicao SE*/
+            {
+                Console.WriteLine("Voce possui mais de 18 anos. Pode entrar.");
+            }
+            else /*Condicao SENAO*/
+            {
+                if (qtdPessoa >= 2) 
+                {
+                    Console.WriteLine("voce nao tem 18, mas pode entrar, "
+                  + "pois está acompanhado");
+                }
+                else
+                {
+                    Console.WriteLine("Voce não possui 18 anos, não pode entrar");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ 1_Alura/Projetos_Solucoes/1-Condicionais/Condicionais2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idadeLuci = 15; /*Declarando variavel*/
+            int qtdPessoa = 2;
+
+            Console.WriteLine("Testando Condicionais 2");
+
+            if (idadeLuci >= 18) /*condicao SE*/
+            {
+                Console.WriteLine("Voce possui mais de 18 anos. Pode entrar.");
+            }
+            else /*Condicao SENAO*/
+
+            {
+                Console.WriteLine("Voce não possui 18 anos, não pode entrar");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
