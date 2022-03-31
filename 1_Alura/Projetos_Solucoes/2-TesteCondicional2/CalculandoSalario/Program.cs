0515773fc133fe1312c5deaf06d1c16ff381ca23
Luci Sousa <marialuci148@gmail.com>
2022-03-30 21:56:54 -0300

    Subindo novos arquivos projetos_Solucoes

---
+++ 1_Alura/Projetos_Solucoes/2-TesteCondicional2/CalculandoSalario/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        bool foiPromovido = true;
+        double salario = 3800.0;
+
+        if (foiPromovido)
+        {
+            Console.WriteLine(salario);
+        }
+        else
+        {
+            Console.WriteLine(salario);
+
+        }
+        Console.ReadLine();
+    }
+
+
+
+}
---
+++ 1_Alura/Projetos_Solucoes/2-TesteCondicional2/ImpostoDeRenda/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpostoDeRenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarando variavel
+            double salario = 3830.0;
+
+            /*De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142;
+              De 2800.01 até 3751.0, o IR é de 15% e pode deduzir R$ 350;  
+              De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636;
+            */
+
+            if (salario >= 1900.0 && salario <= 2800.0)
+            {
+                Console.WriteLine(salario);
+                Console.WriteLine("o IR é de 7.5 % e pode deduzir na declaração o valor de R$ 142");
+                
+            }
+
+            else if (salario >= 2800.01 && salario <= 3751.0)
+            {
+                Console.WriteLine(salario);
+                Console.WriteLine("o IR é de 15% e pode deduzir R$ 350");
+            }
+
+            else if (salario >= 3751.01 && salario <= 4664.00)
+            {
+                Console.WriteLine(salario);
+                Console.WriteLine("o IR é de 22.5% e pode deduzir R$ 636");
+            }
+            Console.ReadLine();
+        }
+    }
+}
