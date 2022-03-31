634a35728182ff47a409b0ece1bc1e0e85a0e120
Luci Sousa <marialuci148@gmail.com>
2022-03-30 22:47:47 -0300

    Subindo novos arquivos projetos_Solucoes

---
+++ Projetos_Solucoes/4-While/TestandoWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //declarando variavel
+        int contador = 1;
+
+        Console.WriteLine("Executando contagem: ");
+        //enquando contador
+        while (contador <= 30)
+        {
+            Console.WriteLine(contador);
+            contador++; //incrementando variavel a partir de 1
+        }
+        Console.ReadLine();
+    }
+}
