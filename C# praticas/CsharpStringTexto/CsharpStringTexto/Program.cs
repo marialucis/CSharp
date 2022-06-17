083c6c7e457c4f32fd3b3293ddf27e8e239367d5
Luci Sousa <marialuci148@gmail.com>
2022-06-17 16:49:13 -0300

    novos exemplos de testes

---
+++ C# praticas/CsharpStringTexto/CsharpStringTexto/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpStringTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variaveis
+
+            string nome;
+            string sexo;
+            string endereco;
+            string telefone;
+            string email;
+            string cpf;
+
+
+
+            Console.WriteLine("-----Cadastro de Pessoas-----");
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("Informe o Nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o CPF: ");
+            cpf = (Console.ReadLine());
+
+            Console.WriteLine("Informe o Sexo: ");
+            sexo = Console.ReadLine();
+
+            Console.WriteLine("Informe o Endereço: ");
+            endereco = Console.ReadLine();
+
+            Console.WriteLine("Informe o Telefone: ");
+            telefone = Console.ReadLine();
+
+            Console.WriteLine("Informe o E-mail: ");
+            email = Console.ReadLine();
+
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Cadastro efetuado com sucesso...\n\n\n\n");
+
+
+            Console.WriteLine("-----Informações do cadastro-----");
+            Console.WriteLine("\n");
+            Console.WriteLine("NOME COMPLETO: " +nome);
+            Console.WriteLine("CPF: " +cpf);
+            Console.WriteLine("SEXO: " +sexo);
+            Console.WriteLine("ENDEREÇO: " +endereco);
+            Console.WriteLine("TELEFONE: " +telefone);
+            Console.WriteLine("E-MAIL: " +email);
+
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ C# praticas/VerificaIdadeFesta/VerificaIdadeFesta/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Aplicação que vai checar se é maior de idade para entrar na festa;
+ * Caso não possua a maior idade, vai checar se está acompanhado;
+ * Se não for maior de idade e não estiver acompanhado, não tem permissão para entrar na festa;
+ * 
+    Desenvolvedor(a): Luci Sousa
+                      17/06/2022  
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaIdadeFesta
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //declarando variaveis
+
+            int idade;
+            int verificaQtdPessoa = 2;
+
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantidade de Pessoas: ");
+            verificaQtdPessoa = int.Parse(Console.ReadLine());
+
+            //condicionais
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Pode entrar! Curta bastante a festa!!");
+            }
+            else if(idade <= 18 && verificaQtdPessoa > 1)
+            {
+                Console.WriteLine("Está acompanhando, pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("É menor de idade e não está acompanhado, não pode entrar!");
+            }
+
+            Console.WriteLine("Próximo...");
+            Console.ReadLine();
+
+        }
+    }
+}
+
