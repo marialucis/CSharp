311db676b60c92435cb0d1024b0ba6e1f4739602
Luci Sousa <marialuci148@gmail.com>
2022-06-19 15:38:39 -0300

    Novas praticas utilizando condicionais if, else, else if e Switch

---
+++ C# praticas/CalculaIR/CalculaIR/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Aplicação que vai verificar o valor do IR a deduzir no salario;
+ 
+    Desenvolvedor(a): Luci Sousa
+                      19/06/2022  
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaIR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142;
+             * De 2800.01 até 3751.0, o IR é de 15% e pode deduzir R$ 350;
+             * De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636;
+            */
+
+            double salario;
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("---------Calcula IR--------");
+            Console.WriteLine("---------------------------");
+
+
+            Console.WriteLine("Informe o seu salario: ");
+            salario = double.Parse(Console.ReadLine());
+
+
+            if (salario >= 1900.0 && salario <= 2800.0)
+            {
+                Console.WriteLine("O IR é de 7.5% e pode deduzir na declaração o valor de R$ 142");
+                
+            }
+            else if (salario >= 2800.01 && salario <= 3751.0 )
+            {
+                Console.WriteLine("O  IR é de 15% e pode deduzir R$ 350");
+            }
+
+
+            else if (salario >= 3751.01 && salario <= 4664.0)
+            {
+                Console.WriteLine("O IR é de 22.5% e pode deduzir R$ 636");
+            }
+
+           Console.ReadLine();
+        }
+
+    }
+    
+}
---
+++ C# praticas/MesesDoAno2/MesesDoAno2/Program.cs
@@ -0,0 +1,122 @@
+﻿/*
+ * Aplicação exibe o mes de acordo com as opções escolhida pelo usuario;
+ * SE a opc  == 1 a aplicação exibe todos os meses;
+ * SENÃO SE opc  == 2 a aplicação pergunta qual mes exibir ao usuario;
+ 
+ * Desenvolvedor(a): Luci Sousa
+                     19/06/2022
+
+
+
+   OBS: Praticando uso dos condicionais IF, ELSE, ELSE IF E SWITCH
+        
+   SOBRE: Essa aplicação foi desenvolvida usando os condicionais IF, ELSE IF E SWITCH para testes de lógica 
+ 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+    
+        int opc; //variavel
+
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine("Escolha uma das opções abaixo: ");
+        Console.WriteLine("\n");
+        Console.WriteLine("1- Exibir todos os meses do ano");
+        Console.WriteLine("2- Exibir um mes especifico");
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Escolha a opção: ");
+        opc = int.Parse(Console.ReadLine());
+
+
+        if (opc == 1)
+        {
+            Console.WriteLine("O mês é Janeiro");
+
+            Console.WriteLine("O mês é Fevereiro");
+
+            Console.WriteLine("O mês é Março");
+
+            Console.WriteLine("O mês é Abril");
+
+            Console.WriteLine("O mês é Maio");
+
+            Console.WriteLine("O mês é Junho");
+
+            Console.WriteLine("O mês é Julho");
+
+            Console.WriteLine("O mês é Agosto");
+
+            Console.WriteLine("O mês é Setembro");
+
+            Console.WriteLine("O mês é Outubro");
+
+            Console.WriteLine("O mês é Novembro");
+
+            Console.WriteLine("O mês é Dezembro");
+
+        }
+        else if (opc == 2)
+        {
+            int mes; //variavel
+            Console.WriteLine("Qual o mes desejado? ");
+            mes = int.Parse(Console.ReadLine());
+            
+            
+            switch (mes)
+            {
+                case 1:
+                    Console.WriteLine("O mês é Janeiro");
+                    break;
+                case 2:
+                    Console.WriteLine("O mês é Fevereiro");
+                    break;
+                case 3:
+                    Console.WriteLine("O mês é Março");
+                    break;
+                case 4:
+                    Console.WriteLine("O mês é Abril");
+                    break;
+                case 5:
+                    Console.WriteLine("O mês é Maio");
+                    break;
+                case 6:
+                    Console.WriteLine("O mês é Junho");
+                    break;
+                case 7:
+                    Console.WriteLine("O mês é Julho");
+                    break;
+                case 8:
+                    Console.WriteLine("O mês é Agosto");
+                    break;
+                case 9:
+                    Console.WriteLine("O mês é Setembro");
+                    break;
+                case 10:
+                    Console.WriteLine("O mês é Outubro");
+                    break;
+                case 11:
+                    Console.WriteLine("O mês é Novembro");
+                    break;
+                case 12:
+                    Console.WriteLine("O mês é Dezembro");
+                    break;
+                default:
+                    Console.WriteLine("Mês inválido");
+                    break;
+            }
+        }
+
+
+        Console.ReadLine();
+    }
+}
