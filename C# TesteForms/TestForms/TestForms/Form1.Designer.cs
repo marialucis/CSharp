614316558c3bc75baa5b68375f75cd164032bb7e
Luci Sousa <marialuci148@gmail.com>
2022-06-28 23:57:48 -0300

    Subindo testes de forms

---
+++ C# TesteForms/TestForms/TestForms/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TestForms
+{
+    partial class frm_Teste
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Titulo = new System.Windows.Forms.Label();
+            this.btn_Sair = new System.Windows.Forms.Button();
+            this.txt_ConteudoLabel = new System.Windows.Forms.TextBox();
+            this.btn_ModificaLabel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Titulo
+            // 
+            this.lbl_Titulo.AutoSize = true;
+            this.lbl_Titulo.Location = new System.Drawing.Point(12, 9);
+            this.lbl_Titulo.Name = "lbl_Titulo";
+            this.lbl_Titulo.Size = new System.Drawing.Size(61, 13);
+            this.lbl_Titulo.TabIndex = 0;
+            this.lbl_Titulo.Text = "Testando...";
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.Location = new System.Drawing.Point(327, 269);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(150, 23);
+            this.btn_Sair.TabIndex = 1;
+            this.btn_Sair.Text = "Fechar aplicação";
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            // 
+            // txt_ConteudoLabel
+            // 
+            this.txt_ConteudoLabel.Location = new System.Drawing.Point(15, 111);
+            this.txt_ConteudoLabel.Name = "txt_ConteudoLabel";
+            this.txt_ConteudoLabel.Size = new System.Drawing.Size(227, 20);
+            this.txt_ConteudoLabel.TabIndex = 2;
+            // 
+            // btn_ModificaLabel
+            // 
+            this.btn_ModificaLabel.Location = new System.Drawing.Point(15, 52);
+            this.btn_ModificaLabel.Name = "btn_ModificaLabel";
+            this.btn_ModificaLabel.Size = new System.Drawing.Size(227, 23);
+            this.btn_ModificaLabel.TabIndex = 3;
+            this.btn_ModificaLabel.Text = "Modifica texto label";
+            this.btn_ModificaLabel.UseVisualStyleBackColor = true;
+            this.btn_ModificaLabel.Click += new System.EventHandler(this.btn_ModificaLabel_Click);
+            // 
+            // frm_Teste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 304);
+            this.Controls.Add(this.btn_ModificaLabel);
+            this.Controls.Add(this.txt_ConteudoLabel);
+            this.Controls.Add(this.btn_Sair);
+            this.Controls.Add(this.lbl_Titulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_Teste";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Titulo;
+        private System.Windows.Forms.Button btn_Sair;
+        private System.Windows.Forms.TextBox txt_ConteudoLabel;
+        private System.Windows.Forms.Button btn_ModificaLabel;
+    }
+}
+
---
+++ C# TesteForms/TestForms/TestForms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForms
+{
+    public partial class frm_Teste : Form
+    {
+        public frm_Teste()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //fecha aplicacao
+
+        }
+
+        private void btn_ModificaLabel_Click(object sender, EventArgs e)
+        {
+            
+                lbl_Titulo.Text = txt_ConteudoLabel.Text;
+            
+
+        }
+    }
+}
