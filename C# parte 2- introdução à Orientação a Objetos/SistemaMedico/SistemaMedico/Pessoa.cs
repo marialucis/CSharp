0c25692baed6b24713eaaa3a870824c8cc62dc74
Luci Sousa <marialuci148@gmail.com>
2022-07-06 17:05:16 -0300

    Novo projeto praticando mais a parte da orientacao a objeto

---
+++ C# parte 2- introdução à Orientação a Objetos/SistemaMedico/SistemaMedico/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMedico
+{
+    public class Pessoa
+    {
+        //atributos da classe
+        public int id;
+        public string nome;
+        public int idade;
+        public double altura;
+        public double peso;
+
+
+    }
+
+    //metodo 
+    
+    
+
+}
---
+++ C# parte 2- introdução à Orientação a Objetos/SistemaMedico/SistemaMedico/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SistemaMedico;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMedico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----------Sistema Medico---------- \n");
+
+            //criando objeto
+            Pessoa pessoa = new Pessoa();
+            pessoa.id = 1;
+            pessoa.nome = "testep";
+            pessoa.idade = 24;
+            pessoa.peso = 60.25;
+            pessoa.altura = 1.52;
+
+            //exbindo informações
+            Console.WriteLine("Dados Paciente \n\n" +
+               "ID: " + pessoa.id + "\n" +
+               "Nome: " + pessoa.nome + "\n" +
+               "Idade: " + pessoa.idade + "\n" +
+               "Peso: " + pessoa.peso + "\n" +
+               "Altura: " + pessoa.altura );
+
+
+            Console.ReadLine();
+        }
+    }
+}
