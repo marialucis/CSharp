a94245f33dbd1ce32025114d29c7bde4162b31fd
marialucis <marialuci148@gmail.com>
2023-11-20 19:02:04 -0300

    subindo provaC#

---
+++ ProvaC#/EstruturaSequencial/Part1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Programador(a): Luci Sousa
+Data: 19/11/23
+
+Açao:
+Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. 
+Calcule e mostre o valor a ser pago.
+
+*/
+
+//variaveis
+int pc1;
+int qtd1 = 0;
+int pc2;
+int qtd2 = 0;
+decimal valorUnitarioPc1 , valorUnitarioPc2, valorAPagar1;
+
+
+
+Console.WriteLine("***------------------***");
+Console.WriteLine("*** Loja Peças Mania ***");
+Console.WriteLine("***------------------***");
+
+Console.WriteLine("Informe o codigo da primeira peça : ");
+pc1 =int.Parse(Console.ReadLine());
+Console.WriteLine("Informe a quantidade de peças: ");
+qtd1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o valor Unitario: ");
+valorUnitarioPc1 = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Informe o codigo da segunda peça : ");
+pc2 =int.Parse(Console.ReadLine());
+Console.WriteLine("Informe a quantidade de peças: ");
+qtd2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o valor Unitario: ");
+valorUnitarioPc2 = decimal.Parse(Console.ReadLine());
+
+valorAPagar1 = (qtd1 * valorUnitarioPc1) + (qtd2 * valorUnitarioPc2);
+
+Console.WriteLine($"Valor a Pagar: {valorAPagar1}");
+
+Console.ReadLine();
---
+++ ProvaC#/EstruturaSequencial/Part2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+Programador(a): Luci Sousa
+Data: 19/11/23
+
+Açao:
+Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo 
+com quatro casas decimais conforme exemplos.
+Fórmula:  = 
+Considere o valor de π = 3.14159
+
+*/
+
+//variaveis
+double pi = Math.Round(3.14159,4);
+double raio ;
+double area;
+
+
+
+
+Console.WriteLine("Digite o valor do raio: ");
+raio = double.Parse(Console.ReadLine());
+
+area = (pi * (raio * raio));
+
+Console.WriteLine($"Area: {area}");
+
+Console.ReadLine();
